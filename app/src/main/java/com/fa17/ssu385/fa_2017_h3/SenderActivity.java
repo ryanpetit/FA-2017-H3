ff3248ba2ea06eebe71191ef0a4aae61b978c480
bhubbells22 <bhubbells22@gmail.com>
2017-09-18 12:26:18 -0700

    completed homework

--- app/src/main/java/com/fa17/ssu385/fa_2017_h3/SenderActivity.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h3/SenderActivity.java
@@ -2,6 +2,7 @@ package com.fa17.ssu385.fa_2017_h3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public class SenderActivity extends AppCompatActivity {
 
