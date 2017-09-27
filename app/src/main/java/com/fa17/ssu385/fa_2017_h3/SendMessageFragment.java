fbef27d0a5ec652efde2acb1667c9a32a044eee0
Kevin Gantsweg <gantsweg@sonoma.edu>
2017-09-26 21:04:11 -0700

    saved progress for 3b

--- app/src/main/java/com/fa17/ssu385/fa_2017_h3/SendMessageFragment.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h3/SendMessageFragment.java
@@ -1,11 +1,14 @@
 package com.fa17.ssu385.fa_2017_h3;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -18,12 +21,36 @@ public class SendMessageFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private EditText supremeNameText;
+    private EditText infidelMessageText;
+    private Button sendMessageButton;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_send_message, container, false);
+        View view = inflater;
+        supremeNameText = view.findViewById(R.id.supreme_edit_text);
+        infidelMessageText = view.findViewById(R.id.infidel_edit_text);
+        sendMessageButton = view.findViewById(R.id.send_message_button);
+
+        sendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                public String nameString;
+                public String messageString;
+                nameString = nameInput.getText().toString();
+                messageString = messageInput.getText().toString();
+            }
+        });
+        return view;
+        //return view.inflate(R.layout.fragment_send_message, container, false);
     }
 
 }
