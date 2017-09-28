411d38ddbcb33ef40c4fcd2bb684e2f28eef5f37
Hayley Gerard <hayleygerard@wrls-103-223.sonoma.edu>
2017-09-27 20:04:11 -0700

    completed homework b

--- app/src/main/java/com/fa17/ssu385/fa_2017_h3/SendMessageFragment.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h3/SendMessageFragment.java
@@ -1,11 +1,15 @@
 package com.fa17.ssu385.fa_2017_h3;
 
 
+import android.app.job.JobScheduler;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -14,16 +18,43 @@ import android.view.ViewGroup;
 public class SendMessageFragment extends Fragment {
 
 
+    private EditText Edit1;
+    private EditText Edit2;
+    private Button Button1;
+    private OnSendButtonClick sendButtonClick;
+
+
     public SendMessageFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        sendButtonClick = (SenderActivity)getActivity();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_send_message, container, false);
+        View view = inflater.inflate(R.layout.fragment_send_message, container, false);
+        Edit1 = view.findViewById(R.id.Edit1);
+        Edit2 = view.findViewById(R.id.Edit2);
+        Button1 = view.findViewById(R.id.Button1);
+        Button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = Edit1.getText().toString();;
+                String message = Edit2.getText().toString();
+                sendButtonClick.onClick(name, message);
+
+            }
+        });
+        return view;
+    }
+
+    public interface OnSendButtonClick{
+        void onClick(String senderName, String senderMessage);
     }
 
 }
