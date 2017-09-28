c9baabd59d3a9be33f202bed18fcffa73bec9e5c
ezquire <tyler.gearing@gmail.com>
2017-09-27 19:20:03 -0700

    completed  homework

--- app/src/main/java/com/fa17/ssu385/fa_2017_h3/ReceiveMessageFragment.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h3/ReceiveMessageFragment.java
@@ -6,13 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ReceiveMessageFragment extends Fragment {
+    public static final String NAME_KEY = "nameKey";
+    public static final String MESSAGE_KEY = "messageKey";
 
+    private TextView nameReceipt;
+    private TextView messageText;
 
     public ReceiveMessageFragment() {
         // Required empty public constructor
@@ -23,7 +28,20 @@ public class ReceiveMessageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_receive_message, container, false);
+        View view = inflater.inflate(R.layout.fragment_receive_message, container, false);
+
+        nameReceipt = (TextView) view.findViewById(R.id.name_receipt);
+        messageText = (TextView) view.findViewById(R.id.message_text);
+
+        if (getArguments() != null) {
+            if (getArguments().containsKey(NAME_KEY)) {
+                nameReceipt.setText(getArguments().getString(NAME_KEY));
+            }
+            if (getArguments().containsKey(MESSAGE_KEY)) {
+                messageText.setText(getArguments().getString(MESSAGE_KEY));
+            }
+        }
+        return view;
     }
 
 }
