5935288e6e477b80bf52e4a25b0770011d610fba
student <student@ssu-cs-dar25-167-143.sonoma.edu>
2017-09-28 18:24:29 -0700

    working on 3b

--- app/src/main/java/com/fa17/ssu385/fa_2017_h3/SendMessageFragment.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h3/SendMessageFragment.java
@@ -1,11 +1,14 @@
 package com.fa17.ssu385.fa_2017_h3;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -13,9 +16,23 @@ import android.view.ViewGroup;
  */
 public class SendMessageFragment extends Fragment {
 
+    private EditText send_edit11;
+    private EditText send_edit22;
+    private Button   send_button;
+
 
     public SendMessageFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onAttachFragment(Fragment childFragment) {
+        super.onAttachFragment(childFragment);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
     }
 
 
@@ -23,7 +40,19 @@ public class SendMessageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_send_message, container, false);
+        View view = inflater.inflate(R.layout.fragment_send_message, container, false);
+        send_edit11 = view.findViewById(R.id.send_text1);
+        send_edit22 = view.findViewById(R.id.send_text2);
+        send_button = view.findViewById(R.id.send_button);
+        send_button.setOnClickListener(new View.OnClickListener());
+
+                @Override
+                public void onClick(View v) {
+                    string name_input;
+                    string message_input;
+                    name_input = send_edit11.getText().toString();
+        }
+        return view;
     }
 
 }
