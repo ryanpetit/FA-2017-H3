a1231a11b93c90ea9d3a98ba3dfa4551547b04dc
student <student@ssu-cs-dar28-167-212.sonoma.edu>
2017-09-28 18:13:18 -0700

    completed homework part a + b

--- app/src/main/java/com/fa17/ssu385/fa_2017_h3/SendMessageFragment.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h3/SendMessageFragment.java
@@ -1,29 +1,59 @@
+
+
 package com.fa17.ssu385.fa_2017_h3;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+//Joshua hernandez
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SendMessageFragment extends Fragment {
+    private EditText senderNameEditText;
+    private EditText senderMessageEditText;
+    private Button senderButton;
 
+    private OnSendButtonClick listener;
 
     public SendMessageFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        listener = (SenderActivity) getActivity();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_send_message, container, false);
+        View view = inflater.inflate(R.layout.fragment_send_message, container, false);
+        senderNameEditText = view.findViewById(R.id.sender_name_edit_text);
+        senderMessageEditText = view.findViewById(R.id.sender_message_edit_text);
+        senderButton = view.findViewById(R.id.sender_button);
+
+        senderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = senderNameEditText.getText().toString();
+                String message = senderMessageEditText.getText().toString();
+                listener.onClick(name, message);
+            }
+        });
+        return view;
     }
 
+    public interface OnSendButtonClick {
+        void onClick(String senderName, String senderMessage);
+    }
 }
--- app/src/main/java/com/fa17/ssu385/fa_2017_h3/SenderActivity.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h3/SenderActivity.java
@@ -1,13 +1,31 @@
 package com.fa17.ssu385.fa_2017_h3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class SenderActivity extends AppCompatActivity {
+public class SenderActivity extends AppCompatActivity implements SendMessageFragment.OnSendButtonClick {
+    private SendMessageFragment mySendMessageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sender);
+        mySendMessageFragment = new SendMessageFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.sender_frame_layout, mySendMessageFragment).commit();
+    }
+
+    @Override
+    public void onClick(String senderName, String senderMessage) {
+        Intent receiverIntent = new Intent(this, ReceiverActivity.class);
+        if (senderName != null) {
+            receiverIntent.putExtra(ReceiverActivity.SENDER_NAME_KEY, senderName);
+        }
+        if (senderMessage != null) {
+            receiverIntent.putExtra(ReceiverActivity.SENDER_MESSAGE_KEY, senderMessage);
+        }
+        startActivity(receiverIntent);
     }
 }
