8d70d492407103b40926bbe018a0c87cab210b48
gaybiigno <student@ssu-cs-dar25-167-135.sonoma.edu>
2017-09-29 17:42:09 -0700

    completed homework - in class

--- app/src/main/java/com/fa17/ssu385/fa_2017_h3/ReceiveMessageFragment.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h3/ReceiveMessageFragment.java
@@ -1,11 +1,13 @@
 package com.fa17.ssu385.fa_2017_h3;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -18,9 +20,18 @@ public class ReceiveMessageFragment extends Fragment {
 
     private TextView nameReceipt;
     private TextView messageText;
+    private Button replyButton;
+
+    private OnReplyButtonClick listener;
 
     public ReceiveMessageFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        listener = (OnReplyButtonClick) getActivity();
     }
 
 
@@ -32,6 +43,7 @@ public class ReceiveMessageFragment extends Fragment {
 
         nameReceipt = (TextView) view.findViewById(R.id.name_receipt);
         messageText = (TextView) view.findViewById(R.id.message_text);
+        replyButton = (Button) view.findViewById(R.id.reply_button);
 
         if (getArguments() != null) {
             if (getArguments().containsKey(NAME_KEY)) {
@@ -41,7 +53,17 @@ public class ReceiveMessageFragment extends Fragment {
                 messageText.setText(getArguments().getString(MESSAGE_KEY));
             }
         }
+        // onclick
+        replyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onClick();
+            }
+        });
         return view;
     }
 
+    public interface OnReplyButtonClick {
+        void onClick();
+    }
 }
--- app/src/main/java/com/fa17/ssu385/fa_2017_h3/ReceiverActivity.java
+++ app/src/main/java/com/fa17/ssu385/fa_2017_h3/ReceiverActivity.java
@@ -1,13 +1,17 @@
 package com.fa17.ssu385.fa_2017_h3;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class ReceiverActivity extends AppCompatActivity {
+public class ReceiverActivity extends AppCompatActivity implements ReceiveMessageFragment.OnReplyButtonClick, SendMessageFragment.OnSendButtonClick {
     public static String SENDER_NAME_KEY = "senderNameKey";
     public static String SENDER_MESSAGE_KEY = "senderMessageKey";
+
     private ReceiveMessageFragment receiverMessageFragment;
+    private SendMessageFragment sendMessageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +31,26 @@ public class ReceiverActivity extends AppCompatActivity {
         receiverMessageFragment.setArguments(args);
 
         getSupportFragmentManager().beginTransaction().add(R.id.receiver_fragment_container, receiverMessageFragment).commit();
+    }
+
+    @Override
+    public void onClick() {
+        sendMessageFragment = new SendMessageFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.receiver_fragment_container, sendMessageFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onClick(String senderName, String senderMessage) {
+        Intent receiverIntent = new Intent(this, ReceiverActivity.class);
+        if (senderName != null) {
+            receiverIntent.putExtra(ReceiverActivity.SENDER_NAME_KEY, senderName);
+        }
+        if (senderMessage != null) {
+            receiverIntent.putExtra(ReceiverActivity.SENDER_MESSAGE_KEY, senderMessage);
+        }
+        startActivity(receiverIntent);
     }
 }
